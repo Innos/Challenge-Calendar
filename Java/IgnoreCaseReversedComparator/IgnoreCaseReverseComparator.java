d6bb4b66d1efb95704e56e07450a95760580c7ea
Innos <victor.d.kazakov@gmail.com>
2015-11-14 22:29:34 +0200

    added inversed ignore case comparator

---
+++ Java/IgnoreCaseReversedComparator/IgnoreCaseReverseComparator.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class IgnoreCaseReversedComparator {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>() {{
+            add("A");
+            add("a");
+            add("B");
+            add("b");
+            add("C");
+            add("c");
+        }};
+
+        Collections.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result = Integer.compare(o1.length(), o2.length());
+                if (result == 0) {
+                    for (int i = 0; i < o1.length(); i++) {
+                        char a = Character.toLowerCase(o1.charAt(i));
+                        char b = Character.toLowerCase(o2.charAt(i));
+                        result = Integer.compare(a, b);
+                        if (result == 0) {
+                            result = -Integer.compare(o1.charAt(i), o2.charAt(i));
+                        }
+                    }
+                }
+                return result;
+            }
+        });
+        System.out.println(strings);
+    }
+}
